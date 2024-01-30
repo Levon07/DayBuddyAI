e9c6ae08aa02547f59e532d8dabc8c15b0681fb7
Levon Nazinyan <nazinyanlevon@gmail.com>
2024-01-30 23:46:22 +0400

    Ready activity_main.xml UI

---
+++ app/src/main/java/com/example/daybuddyai/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.daybuddyai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    LinearLayout popup_menu;
+    public boolean is_popped_up;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        popup_menu = findViewById(R.id.PopUp_Menu_Layout);
+
+    }
+
+    public void ShowPopup(View view){
+        if (!is_popped_up){
+            popup_menu.setVisibility(View.VISIBLE);
+            is_popped_up = true;
+        }else {
+            popup_menu.setVisibility(View.INVISIBLE);
+            is_popped_up = false;
+        }
+
+    }
+}
