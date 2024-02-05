665a46d27a92da16359e79f5e3f083ba51005b2b
Levon Nazinyan <nazinyanlevon@gmail.com>
2024-02-05 17:06:40 +0400

    Developing Menu UI

--- app/src/main/java/com/example/daybuddyai/Themes.java
+++ app/src/main/java/com/example/daybuddyai/Themes.java
@@ -1,8 +1,18 @@
 package com.example.daybuddyai;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.security.AccessController.getContext;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.res.Configuration;
+import android.health.connect.datatypes.units.Length;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Themes extends AppCompatActivity {
 
@@ -11,4 +21,14 @@ public class Themes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themes);
     }
+
+    public void Set_Theme(View view) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+    }
+
 }
